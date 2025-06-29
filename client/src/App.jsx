6f65c580c2addfa45fb0088f6d8f9bb46422a008
Aryan Gadhe <136789464+aryangadhe@users.noreply.github.com>
2025-06-29 22:04:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../constest/AuthContest'
 const App = () => {
   const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./bgImage.svg')]
+    <div className="bg-[url('/bgImage.svg')]
     bg-contain">
       <Toaster/>
       <Routes>
